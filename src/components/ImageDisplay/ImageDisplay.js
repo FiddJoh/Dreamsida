b559f698476c3ff8970bb9928de4377b42b27b9b
Fredrik Josefsson <frjo1904@student.miun.se>
2022-04-28 10:28:19 +0200

    files fetched automatically

--- src/components/ImageDisplay/ImageDisplay.js
+++ src/components/ImageDisplay/ImageDisplay.js
@@ -2,6 +2,7 @@ import ShowImageComponent from 'components/ShowImageComponent/ShowImageComponent
 import React, { Component, Fragment} from 'react';
 import styles from './ImageDisplay.css'
 import 'react-slideshow-image/dist/styles.css';
+import {Slideshow, SlideshowItem } from 'components/Slide/SlideContainer'
 
 function importAll(r) {
     let images = {};
@@ -11,51 +12,39 @@ function importAll(r) {
 
 
 export default class ImageDisplay extends Component {
-        images;
+        images = {};
         fs;
         constructor(props){
             super(props);
 
             
-           this.images = importAll(require.context('../../../public/image/2020', false, /\.(png|jpe?g|svg)$/));
-           console.log(this.images[0]);
+           this.images2020 = importAll(require.context('../../../public/image/2020', false, /\.(png|jpe?g|svg)$/));
+           this.images2021 = importAll(require.context('../../../public/image/2021', false, /\.(png|jpe?g|svg)$/));
+           console.log(this.images);
         }
 
     
-    text = process.env.PUBLIC_URL;
+    text2020 = process.env.PUBLIC_URL + '/image/2020/';
+    text2021 = process.env.PUBLIC_URL + '/image/2021/';
 
-    slideImages = [
-        {
-            url:this.text + '/image/2020/3.png',
-            caption: 'image3'
-        },
-        {
-            url:this.text + '/image/2020/4.png',
-            caption: 'image4'
-        },
-        {
-            url:this.text +'/image/2020/5.png'
-            ,
-            caption: 'image5'
-        },
-        {
-            url:this.text + '/image/2021/1.png'
-            ,
-            caption: 'image1'
-        },
-        {
-            url:this.text + '/image/2021/2.png'
-            ,
-            caption: 'image2'
-        }];
    
 
     render(){        
         return(
             <Fragment>  
-  
-                  <div className={styles.ImageDisplay} id="right-middle">
-                  <ShowImageComponent slideArray={this.slideImages} />
+            <div className={styles.ImageDisplay} id="right-middle">    
+            <Slideshow width={400} height={400}>
+                     {Object.keys(this.images2020).map((key, index)=> (
+                         <SlideshowItem>
+                            <img className="test" src={this.text2020 + key} alt="painting" ></img>
+                        </SlideshowItem>
+                     ))}
+                        {Object.keys(this.images2021).map((key, index)=> (
+                         <SlideshowItem>
+                            <img className="test" src={this.text2021 + key} alt="painting" ></img>
+                        </SlideshowItem>
+                     ))}      
+            </Slideshow>
             </div>
 
             </Fragment>
--- src/components/ShowImageComponent/ShowImageComponent.js
+++ src/components/ShowImageComponent/ShowImageComponent.js
@@ -1,36 +1,64 @@
 import React, { Component} from 'react';
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
-import styles from './ShowImageComponent.css'
 import { Slide } from 'react-slideshow-image';
+import "./ShowImageComponent.css";
 
 
-export default class ShowImageComponent extends React.Component {
-    slideShow = [];
+export default class ShowImageComponent extends Component {
+
     constructor(props){
         super(props);
-        
+        this.slideRef = React.createRef();
+        this.back = this.back.bind(this);
+        this.next = this.next.bind(this);
         this.slideShow = this.props.slideArray;
-
+        this.state = {
+            current: 0
+          };
     }
+    back() {
+        this.slideRef.current.goBack();
+      }
+    
+    next() {
+        this.slideRef.current.goNext();
+      }
     render(){
-        return(
-            <div className={styles.ShowImageComponent} data-testid="ShowImageComponent">
-            <div className="slide-container">
-            <Slide arrows="false">
-             {this.slideShow.map((slideArray, index)=> (
-                <div className="each-fade" key={index}>
-                    <div className="image-container">
-                      <img src = {slideArray.url} alt="test" className ="fadeClass"></img>
-                      </div>
-                    <span>{this.slideShow.caption}</span>
-                  </div>
+        const properties = {
+            duration: 5000,
+            autoplay: true,
+            transitionDuration: 500,
+            arrows: false,
+            infinite: true,
+            easing: "ease",
+            indicators: (i) => <div className="indicator">{i + 1}</div>
+          };
 
+        return(
+            <div className="showImageComponent">
+            <div className="slide-container">
+             <Slide ref={this.slideRef} {...properties}>
+             {this.slideShow.map((slideImage, index)=> (
+                <div className="each-slide" key={index}>
+                <img className="lazy" src={slideImage.url} alt="sample" />
+                {console.log(slideImage.url)}
+                    <span>{slideImage.caption}</span>
+                  </div>
               ))} 
             </Slide>
           </div>
-          </div>
 
+          
+        <div className="slide-container buttons">
+        <button onClick={this.back} type="button">
+          Go Back
+        </button>
+        <button onClick={this.next} type="button">
+          Go Next
+        </button>
+      </div>
+      </div>
         );
     }
 }
